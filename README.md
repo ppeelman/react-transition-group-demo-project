64af5e57cc19b32fdebca3c8e16c2bea48457017
ppeelman <philippe.peelman@gmail.com>
2019-07-30 14:58:57 +0200

    Modal animated using react-transition-group

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 ## react-transition-group
+
+Documentation: https://reactcommunity.org/react-transition-group/
+
+`npm install react-transition-group --save`
+
+### Transition component
+
+There are 4 main states a Transition can be in:
+
+- **'entering'**
+- **'entered'**
+- **'exiting'**
+- **'exited'**
+
+3 mandatory props:
+
+- **in**: Show the component; triggers the enter or exit states (boolean, default: false)
+- **timeout**: The duration of the transition, in milliseconds. Required unless addEndListener is provided.
+- **children**: A function child can be used instead of a React element. This function is called with the current transition status ('entering', 'entered', 'exiting', 'exited'), which can be used to apply context specific props to a component.
+
+Optional props:
+
+- **mountOnEnter**: By default the child component is mounted immediately along with the parent Transition component. If you want to "lazy mount" the component on the first in={true} you can set mountOnEnter. After the first enter transition the component will stay mounted, even on "exited", unless you also specify unmountOnExit. (boolean prop)
+- **unmountOnExit**: By default the child component stays mounted after it reaches the 'exited' state. Set unmountOnExit if you'd prefer to unmount the component after it finishes exiting. (boolean prop)
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Transition } from "react-transition-group";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   };
 
   toggleModalHandler = () => {
@@ -22,8 +24,42 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          onClick={() => {
+            this.setState(prevState => {
+              return {
+                showBlock: !prevState.showBlock
+              };
+            });
+          }}
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {state => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1
+              }}
+            />
+          )}
+        </Transition>
         <Modal show={this.state.modalIsOpen} closed={this.toggleModalHandler} />
-        <Backdrop show={this.state.modalIsOpen} />
+        {this.state.modalIsOpen ? (
+          <Backdrop show={this.state.modalIsOpen} />
+        ) : null}
         <button className="Button" onClick={this.toggleModalHandler}>
           Open Modal
         </button>
